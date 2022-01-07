ace9c5264dacea3f3cbdafafe0d3ddaf94af4459
moros101 <mohitgoswami16@gmail.com>
2022-01-07 22:11:49 +0530

    second commit encryption

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -11,11 +13,14 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
 
+console.log(process.env.API_KEY);
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = new mongoose.model("user", userSchema);
 
 app.get("/", function (req, res) {
@@ -64,10 +69,10 @@ app.post("/login", function (req, res) {
     }
   });
 });
-app.post("/submit",function(req,res){
-  const secret = req.body.secret;
-  
-})
+// app.post("/submit",function(req,res){
+//   const secret = req.body.secret;
+
+// })
 app.listen(3000, function () {
   console.log("http://localhost:3000");
 });
